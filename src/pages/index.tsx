ca9fa88340f7ed78d3a02122130d76699042aa19
sergioescobarh <sergio.escobar.henao@gmail.com>
2023-05-16 20:38:53 -0500

    home in progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-      home
+    <main className='homeMain'>
+      <div className='buttonContainer'>
+        <div className='buttonBorder'>h</div>
+        <button>log in</button>
+        <div className='buttonBorder'>h</div>
+      </div>
+      
+      <button>register</button>
     </main>
   )
 }
